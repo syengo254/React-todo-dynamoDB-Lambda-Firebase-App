dd1f4ac39a25fb130e972a5f815cb93a2e23faed
David Syengo <d.syengo@institute.global>
2023-10-25 19:05:41 +0300

    wip: app is now fully functional, pending firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React, { FormEvent } from 'react';
 import './App.css'
 
 type Task = {
@@ -7,7 +8,9 @@ type Task = {
 }
 
 function App() {
-  const tasks: Task[] = [
+  const [newTask, setNewTask] = React.useState('');
+
+  const [tasks, setTasks] = React.useState<Task[]>([
     {
       id: 1,
       title: "Create a react firebase and firestore app",
@@ -23,10 +26,44 @@ function App() {
       title: "Create a react, aws amplify and dynamoDB apps",
       completed: false,
     },
-  ];
+  ]);
 
-  const onSubmit = (event) => {
-    // todo
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    if (newTask.length < 5) return;
+
+    setTasks((oldTasks) => ([
+      {
+        id: oldTasks.length > 0 ? (oldTasks[oldTasks.length - 1].id + 1) : 1,
+        title: newTask,
+        completed: false
+      },
+      ...oldTasks
+    ]));
+
+    setNewTask('');
+  }
+
+  const removeTask = (taskId: number) => {
+    console.log('removing task with id', taskId);
+    setTasks((oldTasks) => {
+      return oldTasks.filter(({ id }) => id !== taskId);
+    });
+  }
+
+  const markComplete = (taskId: number) => {
+    console.log('task with id', taskId, 'to be marked');
+    setTasks((oldTasks) => {
+      return oldTasks.map((task) => {
+        if (task.id === taskId) {
+          return {
+            ...task, completed: !task.completed
+          }
+        }
+        return task;
+      });
+    });
   }
 
   return (
@@ -37,7 +74,7 @@ function App() {
         <form onSubmit={onSubmit}>
           <div className='task-input'>
             <label htmlFor="task-name">Task name:</label>
-            <input type="text" id="task-name" placeholder='Type your task here' />
+            <input type="text" id="task-name" value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder='Type your task here' />
           </div>
           <div>
             <button type="submit">Add</button>
@@ -46,7 +83,7 @@ function App() {
       </div>
       <div className='task-list'>
         {!tasks.length && <span className='no-tasks'>You have no tasks</span>}
-        {tasks.length > 1 && (
+        {tasks.length > 0 && (
           <ul>
             {
               tasks.map((task, index) => {
@@ -54,9 +91,9 @@ function App() {
                   <li key={task.id + task.title}>
                     <div className='task-name'>
                       <div>{index + 1}. {task.title}</div>
-                      <label>Mark complete: <input type='checkbox' checked={task.completed} /></label>
+                      <label>Mark complete: <input type='checkbox' id={'check' + task.id} checked={task.completed} onChange={() => markComplete(task.id)} /></label>
                     </div>
-                    <button type='button'>&times;</button>
+                    <button onClick={() => removeTask(task.id)} type='button'>&times;</button>
                   </li>
                 )
               })
