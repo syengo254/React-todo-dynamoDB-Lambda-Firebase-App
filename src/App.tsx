be16eba39d9a1f4478177b289e928b03a08930ce
David Syengo <d.syengo@institute.global>
2023-10-25 21:19:03 +0300

    wip: added firebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { FormEvent } from 'react';
 
-import { collection, addDoc, getDocs } from 'firebase/firestore';
+import { collection, addDoc, getDocs, updateDoc, doc, deleteDoc } from 'firebase/firestore';
 
-import { db } from './firebase';
+import { onAuthStateChanged } from 'firebase/auth'
+
+import { db, auth, SignIn, SignOut } from './firebase';
 
 import './App.css';
 
@@ -17,6 +19,8 @@ function App() {
   const [taskTitle, setTaskTitle] = React.useState('');
 
   const [tasks, setTasks] = React.useState<Task[]>([]);
+  const [loading, setLoading] = React.useState(false);
+  const [updating, setUpdating] = React.useState(false);
 
   const onSubmit = async (event: FormEvent) => {
     event.preventDefault();
@@ -29,7 +33,9 @@ function App() {
     }
 
     try {
+      setUpdating(true);
       const docRef = await addDoc(tasksCollectionRef, newTask)
+      setUpdating(false);
       console.log("added new task with ID:", docRef.id);
 
       newTask.id = docRef.id;
@@ -45,53 +51,79 @@ function App() {
     }
   }
 
-  const removeTask = (taskId: Task['id']) => {
+  const removeTask = async (taskId: Task['id']) => {
     console.log('removing task with id', taskId);
+
+    if (!window.confirm("Are you sure?")) return;
+
+    const taskUpdate = doc(tasksCollectionRef, taskId);
+    await deleteDoc(taskUpdate);
+
     setTasks((oldTasks) => {
       return oldTasks.filter(({ id }) => id !== taskId);
     });
   }
 
-  const markComplete = (taskId: Task['id']) => {
-    console.log('task with id', taskId, 'to be marked');
+  const markComplete = async (task: Task) => {
+    console.log('task with id', task.id, 'to be marked');
+
+    const taskUpdate = doc(tasksCollectionRef, task.id);
+    await updateDoc(taskUpdate, { completed: !task.completed });
+
     setTasks((oldTasks) => {
-      return oldTasks.map((task) => {
-        if (task.id === taskId) {
+      return oldTasks.map((itask) => {
+        if (itask.id === task.id) {
           return {
-            ...task, completed: !task.completed
+            ...itask, completed: !itask.completed
           }
         }
-        return task;
+        return itask;
       });
     });
   }
 
-  React.useEffect(() => {
+  onAuthStateChanged(auth, () => {
     const getTasks = async () => {
+      setLoading(true);
       const data = await getDocs(tasksCollectionRef);
       setTasks(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Task[]);
+      setLoading(false);
     };
 
     getTasks();
-  }, []);
+  });
+
+  async function login() {
+    await SignIn();
+  }
+
+  async function logout() {
+    await SignOut();
+  }
 
   return (
     <div>
       <h3>React Todo App + Firebase + Firestore</h3>
+      <div style={{ marginBottom: '.5rem' }}>
+        {auth.currentUser ?
+          <><span>Hi, {auth.currentUser?.displayName}</span><button style={{ marginLeft: '.5rem' }} onClick={logout}>Sign Out</button></> :
+          <button className='login-with-google-btn' onClick={login}>Sign in with Google</button>}
+      </div>
       <div className='task-form'>
         <h4>Create a task</h4>
         <form onSubmit={onSubmit}>
           <div className='task-input'>
             <label htmlFor="task-name">Task name:</label>
-            <input type="text" id="task-name" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} placeholder='Type your task here' />
+            <input type="text" id="task-name" value={taskTitle} onChange={(e) => setTaskTitle(e.target.value)} placeholder='Type your task here' disabled={updating} />
           </div>
           <div>
             <button type="submit">Add</button>
           </div>
         </form>
       </div>
-      <div className='task-list'>
-        {!tasks.length && <span className='no-tasks'>You have no tasks</span>}
+      {auth.currentUser ? <div className='task-list'>
+        <h4 style={{ margin: '0.4rem 0' }}>You tasks:</h4>
+        {!tasks.length && <span className='no-tasks'>{loading ? "Fetching tasks, please wait..." : "You have no tasks"}</span>}
         {tasks.length > 0 && (
           <ul>
             {
@@ -100,7 +132,7 @@ function App() {
                   <li key={task.id + task.title}>
                     <div className='task-name'>
                       <div>{index + 1}. {task.title}</div>
-                      <label>Mark complete: <input type='checkbox' id={'check' + task.id} checked={task.completed} onChange={() => markComplete(task.id)} /></label>
+                      <label>Mark complete: <input type='checkbox' id={'check' + task.id} checked={task.completed} onChange={() => markComplete(task)} /></label>
                     </div>
                     <button onClick={() => removeTask(task.id)} type='button'>&times;</button>
                   </li>
@@ -109,7 +141,8 @@ function App() {
             }
           </ul>
         )}
-      </div>
+      </div> :
+        <div>Sign in first to view your tasks.</div>}
     </div>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +18,24 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
+
 export const db = getFirestore(app);
+
+const provider = new GoogleAuthProvider();
+
+export async function SignIn() {
+    try {
+        return await signInWithPopup(auth, provider);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function SignOut() {
+    try {
+        return await signOut(auth);
+    } catch (e) {
+        console.log(e);
+    }
+}
